89ad68eda1f8947a1ad3af581242a7b51a1dfca1
oleh-andrusiv <andrusiv.o.m@gmail.com>
2022-12-25 23:39:32 +0200

    draft commit 2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,55 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-import App from './App';
+class App extends React.Component {
+  constructor (){
+    super();
+    this.state={
+    toDoList:[], 
+    inputValue:" ",         
+    }
+  }
+  addToDo =()=>{
+    const item={
+      text:this.state.inputValue,
+      isDone:false,
+    }
+    
+    this.setState((prevState)=>{
+      const newToDoList =prevState.toDoList.slice()
+      newToDoList.push(item)
+            return{
+      ...prevState, 
+      toDoList: newToDoList,
+      
+      }
+    })
+
+   }
+  addInputValue =(event)=>{
+     this.setState({inputValue:event.target.value})
+  }
+
+  render (){
+     return (
+    <div className="App">
+      <input type="text" placeholder="Enter please to do" onChange={this.addInputValue} value={this.state.inputValue}></input>
+      <button onClick={this.addToDo}> Submit </button>
+      <ul>
+        {this.state.toDoList.map((el)=>{
+          return (
+            <li>{el.text}</li>
+          )
+
+        })}
+      </ul>
+    </div>
+
+  );
+}
+}
+
+export default App;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
